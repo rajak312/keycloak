f0f6d75266f7d70295a7f138e782c966d34d74d1
Lalit Rajak <lalitrajak312@gmail.com>
2025-09-14 21:33:38 +0530

    moved svg

--- run-keycloakify.sh
+++ run-keycloakify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find all .tsx files under src/admin recursively
-find src/shared -type f -name "*.tsx" | while read file; do
+find src/shared -type f -name "*.svg" | while read file; do
   # Remove "src/" prefix so path starts from admin/
   rel_path="${file#src/}"
   echo "Running keycloakify for: $rel_path"
